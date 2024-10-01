2e0a7361c895900e065039c926df422efb050c3d
Sherlockey <118456803+Sherlockey@users.noreply.github.com>
2024-10-01 13:49:55 -0400

    attack v1 done

--- scripts/attacking.gd
+++ scripts/attacking.gd
@@ -4,26 +4,45 @@ extends PlayerState
 
 
 func enter(previous_state_path: String, data := {}) -> void:
-	# TODO immediately start a timer which will cause a VFX to come out of the attack arc
-	# TODO when that timer is up enable the hitbox then disable the hitbox after another timer
-	player.animation_player.play("attack")
+	if is_equal_approx(player.velocity.x, 0.0):
+		player.animation_player.play("attack")
+	else:
+		player.animation_player.play("attack_running")
 	player.attack_animation_player.play("attack_vfx")
 	await player.animation_player.animation_finished
 	if player.state_machine.state == self:
-		finished.emit(IDLE)
+		if is_equal_approx(player.velocity.x, 0.0):
+			finished.emit(IDLE)
+		else:
+			finished.emit(RUNNING)
 
 
 func physics_update(delta: float) -> void:
+	# Handle movement
 	var input_direction_x := Input.get_axis("move_left", "move_right")
 	player.velocity.x = player.speed * input_direction_x
 	player.velocity.y += player.gravity * delta
 	player.move_and_slide()
 	
+	# Handle object flipping
 	if Input.is_action_pressed("move_left"):
 		player.scale.x = player.scale.y * 1
 	if Input.is_action_pressed("move_right"):
 		player.scale.x = player.scale.y * -1
-
+	
+	# Handle animations
+	if is_equal_approx(input_direction_x, 0):
+		if player.animation_player.current_animation == "attack_running":
+			var interrupted_animation_time : float = player.animation_player.current_animation_position
+			player.animation_player.play("attack")
+			player.animation_player.seek(interrupted_animation_time)
+	else:
+		if player.animation_player.current_animation == "attack":
+			var interrupted_animation_time : float = player.animation_player.current_animation_position
+			player.animation_player.play("attack_running")
+			player.animation_player.seek(interrupted_animation_time)
+	
+	# Handle exit conditions
 	if not player.is_on_floor():
 		finished.emit(FALLING)
 	elif Input.is_action_just_pressed("jump"):
