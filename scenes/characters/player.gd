84cc7dc6a73845f7c6ca2dbf392c79bde4a0f395
Sherlockey <118456803+Sherlockey@users.noreply.github.com>
2024-10-09 10:23:20 -0400

    slowed and damaged in player fixed

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,14 +1,22 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed := 100.0
-@export var acceleration := 1000.00
+@export var speed := speed_base
+@export var acceleration := acceleration_base
 @export var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
-@export var jump_impulse := 325.0
+@export var jump_impulse := jump_impulse_base
 @export var is_vulnerable : bool = true
 
+var is_in_snow : bool = false
 var can_latch : bool = true
 var can_flare : bool = true
+var speed_base : float = 100.0
+var speed_slowed : float = 50.0
+var acceleration_base : float = 1000.0
+var acceleration_slippery : float = 100.0
+var jump_impulse_base: float = 325.0
+var jump_impulse_slowed : float = 185.714
+var damaged_impulse_modifier : float = -1.25
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var attack_animation_player: AnimationPlayer = $AttackAnimationPlayer
@@ -26,6 +34,7 @@ func _physics_process(delta: float) -> void:
 	handle_screen_wrap()
 	check_floor_for_ice()
 	check_floor_for_snow()
+	handle_slowed()
 
 
 func _on_drop_through_timer_timeout() -> void:
@@ -35,27 +44,37 @@ func _on_drop_through_timer_timeout() -> void:
 		drop_through_timer.start()
 
 
+func take_damage() -> void:
+	if is_vulnerable:
+		damaged_animation_player.play("damaged")
+		# Knock the player up and away from current x velocity. 
+		# Could instead take location of damage and move away from that
+		velocity.x = damaged_impulse_modifier * velocity.x
+		velocity.y = -jump_impulse_slowed
+
+
 func handle_screen_wrap() -> void:
 	position.x = wrapf(position.x, 0, screen_size.x)
 
 
 func check_floor_for_ice() -> void:
 	if ice_floor_shape_cast_2d.is_colliding():
-		acceleration = 100.0
+		acceleration = acceleration_slippery
 	else:
-		acceleration = 1000.0
+		acceleration = acceleration_base
 
 
 func check_floor_for_snow() -> void:
 	if snow_floor_shape_cast_2d.is_colliding():
-		print("snow")
-		speed = 50.0
-		jump_impulse = 325.0/1.75
+		is_in_snow = true
 	else:
-		speed = 100.0
-		jump_impulse = 325.0
+		is_in_snow = false
 
 
-func take_damage() -> void:
-	if is_vulnerable:
-		damaged_animation_player.play("damaged")
+func handle_slowed() -> void:
+	if is_in_snow or not is_vulnerable:
+		speed = speed_slowed
+		jump_impulse = jump_impulse_slowed
+	else:
+		speed = speed_base
+		jump_impulse = jump_impulse_base
